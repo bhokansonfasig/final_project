1f0bd931366dd0dc0cbdf444b7522f03efed90ad
René Reimann <renereimann@github.com>
2017-08-31 16:00:25 +0200

    include include guard, added missing imports added functions that are called in run.cpp

--- cpp/include/file_reader.hpp
+++ cpp/include/file_reader.hpp
@@ -2,6 +2,22 @@
  * Stub for a file reader.
  */
 
-class file_reader : public module {
+#ifndef FILE_READER_H_INCLUDED
+#define FILE_READER_H_INCLUDED
 
+#include <module.hpp>
+#include <string>
+
+using std::string;
+
+class file_reader : public module {
+	public:
+		void set_input_filename(string path)
+		{
+			_path = path;
+		}
+	private:
+		string _path;
 };
+
+#endif
