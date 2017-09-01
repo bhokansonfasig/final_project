d82f0a06d672350bf1dded5a472af61f1f9d4a0c
Ben Hokanson-Fasig <bhokansonfasig@gmail.com>
2017-09-01 14:29:33 -0500

    Implemented Frame class with FrameObject Added add, get, remove, and keys functions to Frame class. Added get_pretty_name to FrameObject.

--- cpp/include/frame.hpp
+++ cpp/include/frame.hpp
@@ -3,6 +3,84 @@
  */
 #pragma once
 
+#include <string>
+#include <map>
+#include <memory>
+#include <vector>
+
+#include <iostream>
+
+
+class FrameObject {
+  protected:
+    std::string pretty_name_ = "Default Frame Object";
+
+  public:
+    const std::string get_pretty_name() const { return pretty_name_; }
+};
+
+std::ostream& operator<<(std::ostream& os, const FrameObject& fo)
+{
+  return os << fo.get_pretty_name();
+}
+
+
+
 class Frame {
+  private:
+    std::map<std::string, std::shared_ptr<FrameObject> > object_map_;
+
+  public:
+    // Constructor
+    Frame() = default;
+    // Destructor
+    ~Frame() = default;
+    // Copy Constructor
+    Frame(const Frame&) = default;
+    // Move Constructor
+    Frame(Frame&&) = default;
+    // Copy Assignment
+    Frame& operator=(const Frame&) = default;
+    // Move Assignment
+    Frame& operator=(Frame&&) = default;
+
+    void add(const std::string& key, std::shared_ptr<FrameObject> object_ptr)
+    {
+      std::cout << "Adding " << key << "...";
+      if (object_map_[key]==nullptr) {object_map_[key] = object_ptr;}
+      else {std::cout << " FAILED";} // Raise an error here?
+      std::cout << std::endl;
+    }
+
+    std::shared_ptr<FrameObject> get(const std::string& key) const
+    {
+      auto iter = object_map_.find(key);
+      if (iter!=object_map_.end()) { return iter->second; }
+      else { return nullptr; }
+    }
+
+    void remove(const std::string& key)
+    { std::cout << "Removing " << key << std::endl; object_map_[key] = nullptr; }
+
+    const std::vector<std::string> keys() const
+    {
+      std::vector<std::string> keys;
+      for (auto const& map_pair : object_map_)
+      {
+        if (map_pair.second!=nullptr) { keys.push_back(map_pair.first); }
+      }
+      return keys;
+    }
 
 };
+
+std::ostream& operator<<(std::ostream& os, const Frame& f)
+{
+  os << "Frame:";
+  for (const std::string& key : f.keys())
+  {
+    auto obj = f.get(key);
+    os << "\n\t" << key << ":\t" << *obj << " (" << obj << ")";
+  }
+  return os;
+}
